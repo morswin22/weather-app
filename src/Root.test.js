1b5134e32f57be1ea78003ad984f574e28dabdd7
k_krylowicz <kacper@krylowicz.pl>
2020-01-06 00:38:33 +0100

    added toggle button

--- src/Root.test.js
+++ src/Root.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Root from './App';
+import Root from './Root';
 
 test('renders learn react link', () => {
   const { getByText } = render(<Root />);
--- src/components/DarkModeSwitch/DarkModeSwitch.js
+++ src/components/DarkModeSwitch/DarkModeSwitch.js
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import { DarkModeContext } from "../../hooks/DarkModeContext";
+import './index.css';
 
 export default  () => {
     const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
 
     return (
-        <button onClick={toggleDarkMode}>
-            click for {darkMode ? "light mode" : "dark mode"}
-        </button>
+        // <button onClick={toggleDarkMode}>
+        //     click for {darkMode ? "light mode" : "dark mode"}
+        // </button>
+        <label className="toggle">
+            <input type="checkbox" onClick={toggleDarkMode}/>
+            <span></span>
+        </label>
     )
 }
