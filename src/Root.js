38fcbd935fcd3590c3d403fe088f9e2a8390c9f8
k_krylowicz <kacper@krylowicz.pl>
2020-01-05 23:55:42 +0100

    implemented dark-mode

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import GlobalStyle from "./theme/GlobalStyle";
+import ContextWrapper from "./components/ConextWrapper/ContextWrapper";
+import Layout from "./components/Layout/Layout";
 import ListWrapper from "./components/ListWrapper/ListWrapper";
+import DarkModeSwitch from "./components/DarkModeSwitch/DarkModeSwitch";
 
 const Root = () => (
-    <>
-        <GlobalStyle />
-        <ListWrapper />
-    </>
+    <ContextWrapper>
+        <DarkModeSwitch/>
+        <Layout>
+            <GlobalStyle />
+            <ListWrapper />
+        </Layout>
+    </ContextWrapper>
 );
 
 export default Root;
