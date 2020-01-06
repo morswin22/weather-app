748d68c8c460f3678059179a8e6708926f39221b
k_krylowicz <kacper@krylowicz.pl>
2020-01-06 21:19:18 +0100

    fixed api integration

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -14,8 +14,7 @@ const Wrapper = styled.div`
 `;
 
 const API_KEY = '25e7705fa2fad9ccac48355862164f63';
-
-const weekdays = ['mon','tue','wed','thu','fri','sat','sun']
+const weekdays = ['mon','tue','wed','thu','fri','sat','sun'];
 
 class ListWrapper extends React.Component {
    state = {
@@ -26,26 +25,31 @@ class ListWrapper extends React.Component {
       axios.get('https://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=' + API_KEY)
          .then(res => {
             const days = {};
+
             for (let hourData of res.data.list) {
-               const date = new Date(hourData.dt*1000); // times 1000 because we need to convert from a Unix timestamp to time in JavaScript
+               const date = new Date(hourData.dt * 1000);
 
                const hour = date.getHours();
                const day = date.getDate();
-               const name = weekdays[date.getDay()];
+               const name = weekdays[date.getDay() - 1];
 
                if (!days[day]) days[day] = {name, len: 0}
+
                days[day][hour] = {
-                  temp: Math.floor(100*(hourData.main.temp_min+hourData.main.temp_max)/2)/100,
+                  temp: Math.round((hourData.main.temp_min+hourData.main.temp_max)),
                   weather: `http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`
-               }
+               };
+
                days[day].len++;
             }
 
             const list = [];
             const hours = [1,4,7,10,13,16,19,22];
+
             for (let day in days) {
-               const obj = {day: days[day].name}
-               const nextDay = days[parseInt(day)+1]
+               const obj = {day: days[day].name};
+               const nextDay = days[parseInt(day)+1];
+
                if (nextDay && nextDay[1]) {
                   obj.tempNight = nextDay[1].temp
                } 
@@ -53,7 +57,7 @@ class ListWrapper extends React.Component {
                   obj.tempDay = days[day][16].temp
                   obj.weather = days[day][16].weather
                } else {
-                  // if 16 is not avaible then loop for all the hours and pick one...
+
                   for (let hour of hours) {
                      const data = days[day][hour];
                      if (data) {
@@ -62,10 +66,8 @@ class ListWrapper extends React.Component {
                      }
                   }
                }
-               
-               list.push(obj)
+               list.push(obj);
             }
-            
             this.setState({weatherData: list});
          });
    };
